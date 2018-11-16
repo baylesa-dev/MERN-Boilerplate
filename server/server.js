775ad9cc3b276b07934169b3f4ba231a5119fed2
baylesa-Taker <adrien@taker-lyon.fr>
2018-11-16 03:45:53 +0100

    :card_file_box: Connect back-end to mongodb image

--- server/server.js
+++ server/server.js
@@ -1,13 +1,39 @@
 const express = require('express')
 const cors = require('cors')
 const bodyParser = require('body-parser')
-
+const mongoose = require('mongoose')
 const port = 8080
+
+/*
+** MongoDB initialization
+*/
+mongoose.Promise = global.Promise
+mongoose.connect('mongodb://mongodb')
+    .then(() => {
+        console.log('BackEnd succesfully connected to mongo')
+    })
+    .catch(err => {
+        console.error("Backend error ", err.stack)
+        setTimeout(connectWithRetry, 5000)
+})
+const connectWithRetry = () => {
+  console.log('MongoDB connection with retry')
+  return mongoose.connect('mongodb://mongodb')
+}
+
+/*
+** Express initialization
+*/
+
 const app = express();
 
 app.use(cors())
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
+
+/*
+** Router initialization
+*/
 
 app.get('/api/hello', (req, res) => {
   res.send({ express: 'Hello From Express' })
@@ -18,5 +44,5 @@ app.use(function(req, res) {
 });
 
 app.listen(port, function () {
-     console.log("http://localhost:" + port);
+     console.log("API listening on port:" + port);
 });
