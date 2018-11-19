343218795b92e0b577c27fcc0e15668c9bf49f4c
baylesa-Taker <adrien@taker-lyon.fr>
2018-11-19 02:53:27 +0100

    :rocket: Major changes on server Add https server Add socket io connection Add login router Add passport initialization

--- server/server.js
+++ server/server.js
@@ -1,7 +1,16 @@
+const fs = require('fs')
+const path = require('path')
 const express = require('express')
+const session = require('express-session')
 const cors = require('cors')
 const bodyParser = require('body-parser')
 const mongoose = require('mongoose')
+const passport = require('passport')
+const https = require('https')
+const socketio = require('socket.io')
+const passportInit = require('./src/lib/passport.init')
+const { CLIENT_ORIGIN } = require('./config')
+require('dotenv').config()
 
 /*
 ** MongoDB initialization
@@ -27,22 +36,55 @@ const connectWithRetry = () => {
 const app = express();
 const port = process.env.PORT || 8080
 
-app.use(cors())
+app.set('port', port)
+app.use(cors({origin: CLIENT_ORIGIN}))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
+app.use(express.json())
+app.use(session({
+    secret: process.env.SESSION_SECRET,
+    resave: true,
+    saveUnintialized: true
+}))
+
+/*
+** Passport initialization
+*/
+
+app.use(passport.initialize())
+passportInit()
+
+/*
+** HTTPS Server initialization
+*/
+
+const certOptions = {
+    key: fs.readFileSync(path.resolve('./certs/server.key')),
+    cert: fs.readFileSync(path.resolve('./certs/server.crt')),
+    ca: fs.readFileSync(path.resolve('./certs/server.csr'))
+}
+
+const server = https.createServer(certOptions, app)
+
+/*
+** Socket.io initialization
+*/
+
+const io = socketio(server)
+app.set('io', io)
 
 /*
 ** Router initialization
 */
 
-app.get('/api/hello', (req, res) => {
-  res.send({ express: 'Hello From Express' })
-});
+const loginRouter = require("./src/routes/login.routes")
+
+app.use("/login", loginRouter)
 
 app.use(function(req, res) {
     res.status(404).send({url: req.originalUrl + ' not found'})
 });
 
-app.listen(port, function () {
-     console.log("API listening on port:" + port);
+server.listen(port, function () {
+     console.log("Back-End listening on", port)
 });
